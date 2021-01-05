1e6a1367ef9b7b586b39b032ca594a2cfac552ca
iambillmccann <bill.mccann@gmail.com>
2021-01-04 20:21:56 -0500

    Add source file to output file

--- src/app.py
+++ src/app.py
@@ -4,10 +4,36 @@
 
 import os
 
-from utilities import clear_console
+from utilities import clear_console, get_data, save_data
 
-DATA_FOLDER = "../data/"
 METADATA_CATELOG = "metadata.csv"
+MERGED_DATA = "clean_contacts.csv"
+contact_db = dict()
+
+def update_dictionary(contact):
+
+    email = contact[0]
+    if email in contact_db.keys():
+        db_contact = contact_db[email]
+        for i in range(1, 9):
+            db_contact[i] = contact[i] if db_contact[i] is None else db_contact[i]
+        contact_db[email] = db_contact
+    else:
+        contact_db[email] = contact
+
+def map_contact(metadata_item, contact_item):
+
+    new_contact = [ contact_item[int(metadata_item[i])] for i in [4, 2, 3, 5, 6, 7, 8, 9, 10] ]
+    new_contact.append(metadata_item[0])
+    update_dictionary(new_contact)
+    return new_contact
+
+def process_file(metadata_item):
+
+    data = get_data(metadata_item[0])
+    print("Number of records in {file} is {count}".format(file=metadata_item[0], count=len(data)))
+    [map_contact(metadata_item, item) for item in data]
+    return True
 
 if __name__ == "__main__":
 
@@ -30,3 +56,9 @@ if __name__ == "__main__":
     print('')
     print('copyright William McCann 2021')
     print('-----------------------------------------------------------------------')
+
+    metadata = get_data(METADATA_CATELOG)
+    [process_file(item) for item in metadata]
+
+    contacts = [contact_db[item] for item in contact_db]
+    save_data(contacts, MERGED_DATA)
--- src/utilities.py
+++ src/utilities.py
@@ -1,7 +1,11 @@
 # utilities.py
 #
 # Miscellaneous functions used by the application.
-from os import system, name
+import csv
+
+from os import system, name, path
+
+DATA_FOLDER = "./data/"
 
 def clear_console():
     """ clear the screen
@@ -24,3 +28,57 @@ def clear_console():
 	# for mac and linux(here, os.name is 'posix') 
     else: 
         _ = system('clear') 
+
+def get_data(file_name):
+    """ Load the data into a list
+
+    Args:
+        file_name    The name of the file containing the data (the ranked list)
+
+    Returns:
+        data         A list of items in ranked order
+    """
+
+    if not path.isfile(DATA_FOLDER + file_name): return []
+
+    with open(DATA_FOLDER + file_name, mode = "r") as file:
+
+        data = []
+        first = True
+        csvFile = csv.reader(file)
+        for item in csvFile:
+            if first:
+                first = False
+            else:
+                data.append(item)
+
+    return data
+
+def save_data(data, file_name):
+    """ Save the data into a csv file
+
+    Args:
+        data         A list containing the items to save
+        file_name    The name of the file to contain the data
+    Returns:
+        Nothing
+    """
+
+    fields = [
+        "email",
+        "first-name",
+        "last-name",
+        "primary-phone",
+        "other-phone",
+        "street-address",
+        "city",
+        "state",
+        "zip-code",
+        "source-file"
+    ]
+
+    with open(DATA_FOLDER + file_name, "w") as file:
+        write = csv.writer(file)
+        write.writerow(fields)
+        for item in data:
+            write.writerow(item)
